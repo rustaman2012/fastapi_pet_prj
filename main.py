ee642b4f425e42fb298ebe06db10306d964acfa8
Kaminskyi <kaminsyjj270396@gmail.com>
2024-04-09 23:39:18 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from database import create_tables, delete_tables
+from router import router as tasks_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load the ML model
+    # await delete_tables()
+    # print("База очищена")
+    await create_tables()
+    print("База готова до роботи")
+    yield
+    print("Вимкнення")
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
